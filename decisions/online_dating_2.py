400ea669df762f0af0ccc363a32c84015ac66fc7
echalmers <echalmers@mtroyal.ca>
2024-10-30 14:18:04 -0600

    more decision examples

--- decisions/online_dating_2.py
+++ decisions/online_dating_2.py
@@ -12,12 +12,12 @@ def is_broke() -> bool:
     return input('are you broke? (y/n)') == 'y'
 
 
-# result = 'no match'
-# if is_kind() or smells_good():
-#         if not is_loud():
-#             if not is_broke():
-#                 result = 'match'
-# print(result)
+result = 'no match'
+if is_kind() or smells_good():
+        if not is_loud():
+            if not is_broke():
+                result = 'match'
+print(result)
 
 # as a 1-liner
 result = 'no match'
